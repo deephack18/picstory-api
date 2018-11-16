332e98d283b87ba089dc0cfe378109372c08d374
hlibbabii <hlibbabii@gmail.com>
2018-11-16 23:07:19 +0100

    implement rest of api endpoints

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!venv/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from backend import ImageData
 
@@ -7,14 +7,25 @@ app = Flask(__name__)
 
 image_data = None
 
-@app.route('/api/v1.0/get-points/<user_id>', methods=['GET'])
-def get_points(user_id):
+@app.route('/api/v1.0/get-points/<userId>', methods=['GET'])
+def get_points(userId):
     return jsonify({'points': 178})
 
 
 @app.route('/api/v1.0/check-location/<lng>/<lat>', methods=['GET'])
-def cehck_location(lng, lat):
-    return image_data.get_image_for_location(lng, lat)
+def check_location(lng, lat):
+    challenge_id, encoded_img = image_data.get_image_for_location(lng, lat)
+    if encoded_img is not None:
+        return jsonify({"found": 1, "image": encoded_img, "challengeId": challenge_id})
+    else:
+        return jsonify({"found": 0})
+
+@app.route('/api/v1.0/submit-challenge-photo', methods=['POST'])
+def submit_challenge_photo():
+    encoded_image = request.json["img"]
+    challenge_id = request.json["challengeId"]
+    result = image_data.compare_images(image_data.challenges[challenge_id], encoded_image)
+    return jsonify({"challengeId": challenge_id, "points": result})
 
 
 if __name__ == '__main__':
--- backend.py
+++ backend.py
@@ -1,6 +1,9 @@
 import json
+import threading
 
 import geopy
+
+import imageproc
 
 PATH_TO_DATA = "data.json"
 PATH_TO_IMAGES = "images"
@@ -11,13 +14,35 @@ import base64
 
 class ImageData(object):
     def __init__(self):
+        self.counter = 0
         with open(PATH_TO_DATA, 'r') as f:
             self.image_data = json.load(f)
+        self.challenges = {}
         print(self.image_data['docs'][0])
+        with open('points', 'r') as f:
+            self.points = int(f.readline())
+
+
+    def __get_next_counter(self):
+        with threading.Lock():
+            self.counter += 1
+            return self.counter
 
     def get_image_for_location(self, lng, lat):
         for img_data in self.image_data['docs']:
             if geopy.distance.distance(*img_data['CP_geo'], (lng, lat)).km < DISTANCE_IN_KM_TO_CONSIDER_CLOSE:
                 with open(f'{PATH_TO_IMAGES}/{img_data["B1p"]}', "rb") as image_file:
-                    return base64.b64encode(image_file.read())
+                    encoded_img = base64.b64encode(image_file.read())
+                    challenge_id = self.__get_next_counter()
+                    self.challenges[challenge_id] = {encoded_img}
+                    return challenge_id, encoded_img
         return None
+
+    def __save_points(self):
+        with open('points', 'w') as f:
+            f.write(self.points)
+
+    def compare_images(self, img1, img2):
+        result = imageproc.compare_images(img1, img2)
+        self.points += result
+        self.__save_points()
