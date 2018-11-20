ce2556da1e7ce8a6f8b7bf3bf5b0a2ae9229b62c
hlibbabii <hlibbabii@gmail.com>
2018-11-20 18:59:15 +0100

    minor change

--- load-images.py
+++ load-images.py
@@ -6,12 +6,13 @@ from config import TEST_MODE
 
 
 def load_images(file):
+    print(f"Loading images from {file}...")
     with open(file, 'r') as f:
         data = json.load(f)
         for doc in data['docs']:
             filename = doc["B1p"][0]
             urllib.request.urlretrieve(doc['B1p_url'][0], f'{PATH_TO_IMAGES}/{filename}')
 
-load_images(PATH_TO_DATA)
 if TEST_MODE:
     load_images(PATH_TO_TEST_DATA)
+load_images(PATH_TO_DATA)
